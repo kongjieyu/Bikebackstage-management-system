97bc369f99252c182693a105f680cc69c4bb50e6
May Kong <1285235080@qq.com>
2019-03-01 22:06:12 +1300

    Login

--- my-app/src/pages/form/login.js
+++ my-app/src/pages/form/login.js
@@ -1,14 +1,27 @@
 import React, { Component }  from 'react';
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message, Icon, Checkbox } from 'antd'
 const FormItem = Form.Item;
 
-export default class FormLogin extends Component{
+class FormLogin extends Component{
     constructor(props){
         super(props);
         this.state = {}
     }
-
+    handleSubmit = ()=>{
+        //this.props.form是获取当前的表单
+        //getFieldsValue()可以获取当前表单的value
+        //然后把这个obj对象赋给userInfo
+        let userInfo = this.props.form.getFieldsValue();
+        //去校验字段
+        this.props.form.validateFields((err,values)=>{
+            if(!err){
+                message.success(`${userInfo.userName} 恭喜你，您通过本次表单组件学习，当前密码为：${userInfo.userPwd}`)
+            }
+        })
+    }
     render(){
+        //getFieldDecorator用于表单的封装， js属性对象
+      const {getFieldDecorator, getFieldsError, getFieldError, isFieldTouched} = this.props.form;
         return(
             <div>
                 <Card title="登陆行内表单">
@@ -17,7 +30,7 @@ export default class FormLogin extends Component{
                             <Input placeholder="请输入用户名"/>
                         </FormItem>
                         <FormItem>
-                            <Input placeholder="请输入密码" />
+                                <Input placeholder="请输入密码" />
                         </FormItem>
                         <FormItem>
                             <Button type="primary">登录</Button>
@@ -27,13 +40,46 @@ export default class FormLogin extends Component{
                 <Card title="登陆行内表单" style={{marginTop:10}}>
                     <Form style={{width: 300}}>
                         <FormItem>
-                            <Input placeholder="请输入用户名"/>
+                            {getFieldDecorator('userName', {
+                                initialValue: 'May',
+                                rules: [{ 
+                                    required: true, 
+                                    message: 'Please input your username!' },
+                                        {
+                                            min:3,max:10,
+                                            message:'长度不在范围内'
+                                        },
+                                        {
+                                            pattern:new RegExp('^\\w+$','g'),
+                                            message:'用户名必须为字母或者数字'
+                                        }],
+                                    })(
+                                    <Input prefix={<Icon type="user"/>} placeholder="请输入用户名"/>
+                                    )}
                         </FormItem>
                         <FormItem>
-                            <Input placeholder="请输入密码" />
+                            {getFieldDecorator('userPwd', {
+                                initialValue: '123456',
+                                rules: [{ 
+                                    required: true, 
+                                    message: 'Please input your Password!' }],
+                                        })(
+                                        <Input  prefix={<Icon type="lock" />} placeholder="请输入密码" />
+                                        )}
                         </FormItem>
                         <FormItem>
-                            <Button type="primary">登录</Button>
+                            {
+                                getFieldDecorator('remember', {
+                                    valuePropName:'checked',
+                                    initialValue: true
+                                })(
+                                    <Checkbox>记住密码</Checkbox>
+                                )
+                            }
+                            <a href="#" style={{float:'right'}}>忘记密码</a>
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" onClick={this.handleSubmit}>登录</Button>
                         </FormItem>
                     </Form>
                 </Card>
@@ -41,3 +87,4 @@ export default class FormLogin extends Component{
         )
     }
 }
+export default Form.create()(FormLogin);
